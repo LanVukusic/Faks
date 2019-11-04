21798e3044d583f5c7ed3e2f739c9756fc70fa5e
Lan <lan.vukusic@gmail.com>
2019-11-04 15:48:33 +0100

    optimized DN3, to run in h+w time, consistently

--- OP1/Dn3/DN03_63190321.java
+++ OP1/Dn3/DN03_63190321.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.lang.Math;
 
 /**
  * DN03_63190321
@@ -20,43 +21,70 @@ public class DN03_63190321 {
             int t = sc.nextInt();
 
             int time = 0;
+
+            int nextFreeX = ax;
+            int nextFreeY = ay;
             
-            while (h != 1 || w !=1){
-                
-                if (isFree(ax, bx, cx, time) && w != 1){  // check if he can go right
-                    // System.out.println("time: "+time+" go right"); 
-                    w -= 1;
-                    // System.out.println(w);
-                    time += t;
+            while (true){ // loop while player has not yet reached (1,1) break on line 44
+                if (isFree(ax, bx, cx, time) && w != 1){  // check if he can go right 
+                    w -= 1; // move him right
+                    nextFreeX = getTimeIncrement(ax, bx, cx, t, time); // when is the next green light?
+                    nextFreeY = getTimeIncrement(ay, by, cy, t, time);
+                    time += t;  // pass the time
                 }else{
                     if(isFree(ay, by, cy, time) && h != 1){  // can he mybe go down than?
-                       //  System.out.println("time: "+time+" go down");
                         h -= 1;
-                        // System.out.println(h);
-                        time += t;
-                    }else{  // no? i guess he will just wait there for 1 "time"
-                        // System.out.println(time);
-                        time ++;
-                        
+                        nextFreeX = getTimeIncrement(ax, bx, cx, t, time); // when is the next green light?
+                        nextFreeY = getTimeIncrement(ay, by, cy, t, time);                        
+                        time += t;  // pass the time
                     }
                 }
-                
-                
+                if(h == 1 && w ==1){  // if we are at school... sweet!!
+                    System.out.println(time);
+                    break;
+                }
+                time = getTime(w, h, nextFreeX, nextFreeY, time); // skip time to next time frame, when we can move
             }
-
-            System.out.println(time);
-            
         }
 
         public static boolean isFree(int a, int b, int c, int time){
             if (time < a){  // if time is within the firs <a> seconds
                return false;  // than we can say it is RED
             }
-            time = (time - a) % (b + c);
-            if (time < b){
-                return true;
+            time = (time - a) % (b + c);  // if its later in time, we can MOD it with sum of a+b to determine absolute position in
+                                        //the red / green loop
+            if (time < b){  // if it is on the beginning of the loop, so before first red :
+                return true;  // it is GREEN
             }else{
-                return false;
+                return false; // if not, it must be RED
+            }
+            
+        }
+
+        public static int getTimeIncrement(int a, int b, int c, int delay, int time ){
+            if (time + delay < b){  // if we havent reached first GREEN that's the one
+                return a;
+            }else{ // that means we are in the green / red loop
+                int o = (time + delay - a) % (b + c); // we reset the loop to the relative times
+                if(o < (b)){  // if we are on GREEN at the moment we stop moving..
+                    return time + delay;  // than sweet!! we can just say thats our time.
+                }else{  // if not, we must calculate it
+                    return (time + delay) + ((b+c)-o);  // we wait additional (b+c)-o time
+                }                                      // time we need to reach the next GREEN
+            }
+        }
+
+        public static int getTime (int px, int py, int nx, int ny, int time){
+            if (px == 1){  // if we cant move on x, we give priorit to y
+                nx = ny;
+            }
+            if (py == 1){  // if we cant move in y, we give priority to x
+                ny = nx;
+            }
+            if(time <= Math.min(nx, ny)){  // if we havent missed our next best time
+                return Math.min(nx, ny);  // nice, lets set time to our next nearest time.
+            }else{  // we missed the next event
+                return Math.max(nx, ny);  // so we move on our second best option
             }
         }
 }
